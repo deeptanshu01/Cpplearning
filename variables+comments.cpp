ad6d0fd008c39490a4fef9e2079d870dbee49f11
deeptanshu01 <87687372+deeptanshu01@users.noreply.github.com>
2022-01-08 17:15:26 +0530

    Add files via upload

---
+++ variables+comments.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello world";
+    return 0;
+}
+
+
+// Low level: near the computer hardware
+// high level: More logical, user interface 
+// VARIABLES: containers used to store your data
+// types of variables
+// int - integers
+// float -decimals
+// char -characters
+// programming language act as a connection between low level and a high level
+// EVERYTHING HERE HAS BEEN COMMENTED, FOR EXCESS INFORMATION, FOR UNDERSTANDING OF THE PROGRAMMER 
+// 
+// 
