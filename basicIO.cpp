b1a45726ce7ad31390bc7c0fbfdf5c4edb3a2611
deeptanshu01 <87687372+deeptanshu01@users.noreply.github.com>
2022-01-10 12:00:40 +0530

    Add files via upload

---
+++ basicIO.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(   ){
+    int num1,num2;
+    cout<< /*<-this is called insertion operator*/ "ENter num1\n";
+    cin>>/*<- this is extraction operator*/num1;
+    cout<<"Enter num2\n";
+    cin>>num2;
+    cout<<"sum is 12"<<num1+num2;
+    return 0;
+}
