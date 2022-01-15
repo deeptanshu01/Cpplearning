fcee98b083c5bcafff77628ae7e065b1d3ac0d6d
deeptanshu01 <87687372+deeptanshu01@users.noreply.github.com>
2022-01-15 18:23:26 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // loops in C++
+    // 3 types
+    // 1.while
+    // 2.for
+    // 3.do while
+
+    // for loop- print numbers 1 to 100
+    int a;
+    a=0;
+    for(int a=0;a<=10;a++){
+        cout<<a<<endl;
+    }
+    // for(intitialization;condition;updation){
+    // code
+    // }
+
+    // while loops
+    while (a<10)
+    {
+        cout<<a<<endl;
+        a++;
+    }
+    // while(condition){
+        // statements}
+
+
+    // Do while loop
+    
+    return 0;
+}
